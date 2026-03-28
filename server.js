5ab68f6f0895e85460c1b54bc9de4066d6ffc966
Ana <abarba1@trinity.edu>
2026-03-28 22:23:38 +0100

    html + css + placeholder image

--- server.js
+++ server.js
@@ -3,37 +3,37 @@ const express = require('express')
 const app = express()
 app.use(express.static('web'))
 
-// read pet.json
-function loadPet() {
-    return JSON.parse(fs.readFileSync('pet.json', 'utf8'))
+// read ana.json
+function loadAna() {
+    return JSON.parse(fs.readFileSync('ana.json', 'utf8'))
 }
 
 // decay
-function applyDecay(pet){
-    const hungerHours = (Date.now() - pet.lastHunger) / 3600000
-    const happinessHours = (Date.now() - pet.lastHappiness) / 3600000
-    const energyHours = (Date.now() - pet.lastEnergy) / 3600000
-    const evilnessHours = (Date.now() - pet.lastEvilness) / 3600000
+function applyDecay(ana){
+    const hungerHours = (Date.now() - ana.lastHunger) / 3600000
+    const happinessHours = (Date.now() - ana.lastHappiness) / 3600000
+    const energyHours = (Date.now() - ana.lastEnergy) / 3600000
+    const evilnessHours = (Date.now() - ana.lastEvilness) / 3600000
 
-    pet.hunger = Math.max(0, Math.min(100, pet.hunger - (hungerHours * 2 )))
-    pet.happiness = Math.max(0, Math.min(100, pet.happiness - (happinessHours * 2 )))
-    pet.energy = Math.max(0, Math.min(100, pet.energy - (energyHours * 2 )))
-    pet.evilness = Math.max(0, Math.min(100, pet.evilness - (evilnessHours * 2 )))
+    ana.hunger = Math.max(0, Math.min(100, ana.hunger - (hungerHours * 2 )))
+    ana.happiness = Math.max(0, Math.min(100, ana.happiness - (happinessHours * 2 )))
+    ana.energy = Math.max(0, Math.min(100, ana.energy - (energyHours * 2 )))
+    ana.evilness = Math.max(0, Math.min(100, ana.evilness - (evilnessHours * 2 )))
 
-    return pet
+    return ana
 }
 
-// save pet.json back
-function savePet(pet) {
-    fs.writeFileSync('pet.json', JSON.stringify(pet, null, 2))
+// save ana.json back
+function saveAna(ana) {
+    fs.writeFileSync('ana.json', JSON.stringify(ana, null, 2))
 }
 
 // status for webpage
 app.get('/status', (req, res) => {
-    const pet = loadPet()
-    applyDecay(pet)
-    savePet(pet)
-    res.json(pet)
+    const ana = loadAna()
+    applyDecay(ana)
+    saveAna(ana)
+    res.json(ana)
 })
 
 // tanagotchi
@@ -43,49 +43,49 @@ app.get('/tanagotchi', (req, res) => {
 
 // HUNGER ----------
 app.post('/feed', (req, res) => {
-    const pet = loadPet()
-    applyDecay(pet)
+    const ana = loadAna()
+    applyDecay(ana)
 
-    pet.hunger = Math.min(100, pet.hunger + 4)
-    pet.lastHunger = Date.now()
+    ana.hunger = Math.min(100, ana.hunger + 4)
+    ana.lastHunger = Date.now()
 
-    savePet(pet)
+    saveAna(ana)
     res.end()
 })
 
 // HAPPINESS ----------
 app.post('/compliment', '/hug', '/play', (req, res) => {
-    const pet = loadPet()
-    applyDecay(pet)
+    const ana = loadAna()
+    applyDecay(ana)
 
-    pet.happiness = Math.min(100, pet.happiness + 2)
-    pet.lastHappiness = Date.now()
+    ana.happiness = Math.min(100, ana.happiness + 2)
+    ana.lastHappiness = Date.now()
 
-    savePet(pet)
+    saveAna(ana)
     res.end()
 })
 
 //ENERGY ----------
 app.post('/nap', '/kiss', (req, res) => {
-    const pet = loadPet()
-    applyDecay(pet)
+    const ana = loadAna()
+    applyDecay(ana)
 
-    pet.energy = Math.min(100, pet.energy + 2)
-    pet.lastEnergy = Date.now()
+    ana.energy = Math.min(100, ana.energy + 2)
+    ana.lastEnergy = Date.now()
     
-    savePet(pet)
+    saveAna(ana)
     res.end()
 })
 
 // EVILNESS ----------
 app.post('/burp', '/meow', '/raspberry', (req, res) => {
-    const pet = loadPet()
-    applyDecay(pet)
+    const ana = loadAna()
+    applyDecay(ana)
 
-    pet.evilness = Math.min(100, pet.evilness + 2)
-    pet.lastEvilness = Date.now()
+    ana.evilness = Math.min(100, ana.evilness + 2)
+    ana.lastEvilness = Date.now()
 
-    savePet(pet)
+    saveAna(ana)
     res.end()
 })
 
